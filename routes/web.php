08931115866b71b02651ea43d67e76d5fbd86282
Bruno <brunoomendes20@gmail.com>
2026-01-03 12:22:05 -0300

    feat: configure auth and guest route groups

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'create'])->name('register');
+    Route::post('/users', [AuthController::class, 'store'])->name('users.store');
+
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginSubmit'])->name('login.store');
 });
+
+
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/', [TaskController::class, 'index'])->name('home');
+        Route::resource('tasks', TaskController::class)->except('show');
+
+        Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+    }
+);
