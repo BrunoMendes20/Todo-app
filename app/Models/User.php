db1ed076e7293e226781d1719672aa397ea4fbbf
Bruno <brunoomendes20@gmail.com>
2026-01-06 11:46:24 -0300

    refactor(user): add email verification and password visibility rules

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\CustomResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Notifications\ResetPassword as resetPasswordBase;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function tasks()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Task::class);
+    }
+
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password'];
+
+    protected static function booted()
+    {
+        resetPasswordBase::toMailUsing(function ($notifiable, $token) {
+            return (new CustomResetPassword($token))
+            ->toMail($notifiable);
+        });
     }
 }
