8f71b9e9c108bb8145346c8d7068880e0af089fb
Bruno <brunoomendes20@gmail.com>
2026-01-06 11:44:18 -0300

    feat(auth): customize password reset email notification

---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public string $token)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $resetLink = route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->email,
+        ]);
+
+        return (new MailMessage())
+          ->subject('Recuperação de senha')
+          ->view('mail.notification-password', [
+              'user' => $notifiable,
+              'resetLink' => $resetLink,
+          ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
