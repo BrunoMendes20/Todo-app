560a5784281c58d5fa155846d53140ba7e82034a
Bruno <brunoomendes20@gmail.com>
2026-01-15 18:33:55 -0300

    chore(layout): include tasks javascript in app layout

--- resources/views/components/layout-app.blade.php
+++ resources/views/components/layout-app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }}
         @isset($pageTitle)
             {{ $pageTitle }}
@@ -13,14 +14,16 @@
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <script src="https://kit.fontawesome.com/0ed6f7bf05.js" crossorigin="anonymous"></script>
 
 </head>
 
-<body>
+<body class="bg-auth">
     {{ $slot ?? '' }}
 
     <script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/messages.js') }}"></script>
+    <script src="{{ asset('assets/js/tasks.js') }}"></script>
 </body>
 
 </html>
