3b71d5f4f8158c35ddf9de48cada91338865a68d
Bruno <brunoomendes20@gmail.com>
2026-01-15 18:28:03 -0300

    feat(routes): add toggle route for task completion status and comments

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,60 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Routes accessible only to guests (unauthenticated users)
+|--------------------------------------------------------------------------
+*/
 Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'create'])->name('register');
-    Route::post('/users', [AuthController::class, 'store'])->name('users.store');
+    // Registration routes
+    Route::get('/register', [AuthController::class, 'create'])
+        ->name('register');
 
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/login', [AuthController::class, 'loginSubmit'])->name('login.store');
+    Route::post('/users', [AuthController::class, 'store'])
+        ->name('users.store');
 
-    Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotPassword'])->name('password.request');
-    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    // Login routes
+    Route::get('/login', [AuthController::class, 'login'])
+        ->name('login');
 
-    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-    Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
+    Route::post('/login', [AuthController::class, 'loginSubmit'])
+        ->name('login.store');
+
+    // Forgot password routes
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotPassword'])
+        ->name('password.request');
+
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
+        ->name('password.email');
+
+    // Reset password routes
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])
+        ->name('password.reset');
+
+    Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])
+        ->name('password.update');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Routes accessible only to authenticated users
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth')->group(function () {
+    // Home page (task list)
+    Route::get('/', [TaskController::class, 'index'])
+        ->name('home');
 
-Route::middleware('auth')->group(
-    function () {
-        Route::get('/', [TaskController::class, 'index'])->name('home');
-        Route::resource('tasks', TaskController::class)->except('show');
+    // Task CRUD routes (except show)
+    Route::resource('tasks', TaskController::class)
+        ->except('show');
 
-        Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
-    }
-);
+    // Logout route
+    Route::post('/logout', [AuthController::class, 'destroy'])
+        ->name('logout');
+
+    // Toggle task completion status
+    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])
+        ->name('tasks.toggle');
+});
